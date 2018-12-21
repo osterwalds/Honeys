6cd6a1adbf9a3deb43cda29d70347a2bc25ab38b
osterwalds <45991892+osterwalds@users.noreply.github.com>
2018-12-21 09:56:38 +0100

    Update restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,4 +1,18 @@
+# stop all container
 docker stop $(docker ps -aq)
+
+# remove all containers
 docker rm $(docker ps -aq)
-rm /unsafe/logs/smb/smb.log
+
+# remove all logs
+rm -rf /unsafe/logs
+
+# recreate log folders
+mkdir /unsafe/logs
+
+##########################
+#   Starting Containers  #
+##########################
+
+# Start smb container
 docker run -it -d -v /unsafe/logs/smb:/var/log/samba -p 139:139 -p 445:445 steven411/smb
