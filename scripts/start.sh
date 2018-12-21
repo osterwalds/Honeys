a42a3d6708e5ae14daa5d6a06112bc9d1feece52
osterwalds <45991892+osterwalds@users.noreply.github.com>
2018-12-21 16:21:19 +0100

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,10 +9,14 @@
 # add logfile monitor
 /opt/splunkforwarder/bin/splunk add monitor /unsafe/logs/
 
+# add data-sec splunk-server
+/opt/splunkforwarder/bin/splunk start
+
 # run reposync
 sh /home/datasec/scripts/syncrepo.sh
 
 # run firststart
+sh /home/datasec/scripts/Honeys/scripts/firststart.sh
 
 # reboot
 reboot now
