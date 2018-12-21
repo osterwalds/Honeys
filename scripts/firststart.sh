d420d3ec5afc1fb73386cff6c6800cc941061f8e
Steven Osterwald <osterwalds@data-sec.net>
2018-12-21 11:18:18 +0100

    updated firststart.sh

--- scripts/firststart.sh
+++ scripts/firststart.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 # Simple update,upgrade
 sudo apt update && sudo apt upgrade -y
 
@@ -8,13 +10,16 @@ sudo sh /home/datasec/scripts/syncrepo.sh
 clear
 
 # ask for hostname
-echo "Set up the hostname for this VM. This name will show up in Splunk later on"
-echo ""
-echo "Type in new hostname now and press ENTER:"
-read hname
-echo $hname > /etc/hostname
+#echo "Set up the hostname for this VM. This name will show up in Splunk later on"
+#echo ""
+#echo "Type in new hostname now and press ENTER:"
+#read hname
+#echo $hname > /etc/hostname
+
+sudo sh /home/datasec/scripts/Honeys/scripts/restart.sh
 
 # clear screen
+clear
 
 # done and restart
 echo "All done"
