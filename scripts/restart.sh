298ce126a5f1dd8b6f356ea0c3524b215de99c6a
osterwalds <45991892+osterwalds@users.noreply.github.com>
2019-01-02 16:50:33 +0100

    Update restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -32,4 +32,4 @@ docker run -it -d -v /unsafe/logs/smb:/var/log/samba -p 139:139 -p 445:445 steve
 docker run -d -v /unsafe/logs/ftp:/var/log/ftplog -p 20:20 -p 21:21 -e FTP_USER=admin -e FTP_PASS=admin steven411/ftp
 
 # Start mysql container
-docker run -d -v /unsafe/static:/etc/mysql/conf.d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password123 mysql
+docker run -d -v /unsafe/static:/etc/mysql/conf.d -v /unsafe/logs/mysql:/var/log/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password123 mysql
