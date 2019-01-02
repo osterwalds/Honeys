6836c597336c3428640ef63249652f460a7af11e
osterwalds <45991892+osterwalds@users.noreply.github.com>
2019-01-02 16:48:24 +0100

    Update restart.sh

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -15,6 +15,9 @@ rm -rf /unsafe/static
 # recreate static folder
 mkdir /unsafe/static
 
+## copy cnf mysql
+cp /home/datasec/scripts/Honeys/mysql/configlogs.cnf /unsafe/static/configlogs.cnf
+
 # recreate log folders
 mkdir /unsafe/logs
 
@@ -27,3 +30,6 @@ docker run -it -d -v /unsafe/logs/smb:/var/log/samba -p 139:139 -p 445:445 steve
 
 # Start vsftpd container
 docker run -d -v /unsafe/logs/ftp:/var/log/ftplog -p 20:20 -p 21:21 -e FTP_USER=admin -e FTP_PASS=admin steven411/ftp
+
+# Start mysql container
+docker run -d -v /unsafe/static:/etc/mysql/conf.d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password123 mysql
