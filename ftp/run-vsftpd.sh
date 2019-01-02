fac0c14e2ea72b90c7279d05b07678c43eb34bc1
osterwalds <45991892+osterwalds@users.noreply.github.com>
2019-01-02 15:43:38 +0100

    Create run-vsftpd.sh

---
+++ ftp/run-vsftpd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create home dir and update vsftpd user db:
+mkdir -p /home/vsftpd
+chown -R ftp:ftp /home/vsftpd
+echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
+db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
+
+# Run vsftpd:
+vsftpd /etc/vsftpd/vsftpd.conf
