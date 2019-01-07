6e6729eb13293fb43186bd8f9a593a91f577a5b3
osterwalds <45991892+osterwalds@users.noreply.github.com>
2019-01-07 17:39:53 +0100

    Update heartbeat.sh

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -1,3 +1,7 @@
+#! /bin/bash
+
+# Log the ip, current time and heartbeat to the file heartbeat.log
+
 echo "" >> /unsafe/logs/heartbeat.log
 echo "###############################################" >> /unsafe/logs/heartbeat.log
 cat /proc/drivers/rtc | grep rtc_time >> /unsafe/logs/heartbeat.log
