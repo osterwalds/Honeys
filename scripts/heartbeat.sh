41c55e3a7789ff140be115f7b61c6d9a488e2754
osterwalds <45991892+osterwalds@users.noreply.github.com>
2019-03-28 14:10:22 +0100

    Update heartbeat.sh

--- scripts/heartbeat.sh
+++ scripts/heartbeat.sh
@@ -4,7 +4,7 @@
 
 echo "" >> /unsafe/logs/heartbeat.log
 echo "###############################################" >> /unsafe/logs/heartbeat.log
-cat /proc/drivers/rtc | grep rtc_time >> /unsafe/logs/heartbeat.log
+cat /proc/driver/rtc | grep rtc_time >> /unsafe/logs/heartbeat.log
 echo "-----------------------------------------------" >> /unsafe/logs/heartbeat.log
 ip addr show | grep inet >> /unsafe/logs/heartbeat.log
 echo "-----------------------------------------------" >> /unsafe/logs/heartbeat.log
